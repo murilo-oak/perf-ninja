5d6a694974fe511fa7aeac2e724af43d9b48e435
Murilo Carvalho <murilo.carvalho@rilix.com.br>
2026-02-15 00:33:31 -0300

    interchange loop 2 solution 2

--- labs/memory_bound/loop_interchange_2/solution.cpp
+++ labs/memory_bound/loop_interchange_2/solution.cpp
@@ -30,35 +30,11 @@ static void filterVertically(uint8_t *output, const uint8_t *input,
     }
   }
 
-  int dots[width];
-  
-  for(int i = 0; i < width; i++) {
-    dots[i] = 0;
-  }
-  
-  for (int r = radius; r < height - radius; r++) { 
-    for (int i = 0; i < radius + 1 + radius; i++) {
-      for (int c = 0; c < width; c++) {
-      // Accumulation
-        dots[c] += input[(r - radius + i) * width + c] * kernel[i];
-      }
-    }
-
-    for (int c = 0; c < width; c++)
-    {
-      // Fast shift instead of division
-      int value = (dots[c] + rounding) >> shift;
-      output[r * width + c] = static_cast<uint8_t>(value);
-      dots[c] = 0;
-    }
-  }
-
-  /*// Middle part of computations with full kernel
+  // Middle part of computations with full kernel
   for (int r = radius; r < height - radius; r++) {
     for (int c = 0; c < width; c++) {
       // Accumulation
       int dot = 0;
-      
       for (int i = 0; i < radius + 1 + radius; i++) {
         dot += input[(r - radius + i) * width + c] * kernel[i];
       }
@@ -68,7 +44,6 @@ static void filterVertically(uint8_t *output, const uint8_t *input,
       output[r * width + c] = static_cast<uint8_t>(value);
     }
   }
-  */
 
   // Bottom part of line, partial kernel
   for (int r = std::max(radius, height - radius); r < height; r++) {
